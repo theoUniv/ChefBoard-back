5726c82fdff88c2d41c2a01859e359f6cb6f00c1
theoUniv <149110726+theoUniv@users.noreply.github.com>
2025-06-13 13:46:43 +0200

    ee

--- chefboard-back/src/controllers/authController.js
+++ chefboard-back/src/controllers/authController.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 // # Générer un token JWT #
 const generateToken = (user) => {
-    return jwt.sign({ id: user._id, email: user.email, role: user.role }, process.env.JWT_SECRET, {
+    return jwt.sign({ id: user._id, email: user.email, role: user.role, first_name: user.first_name, last_name: user.last_name, favorite_categories: user.favorite_categories, created_at: user.created_at }, process.env.JWT_SECRET, {
         expiresIn: '24h'
     });
 };
